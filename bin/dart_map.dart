24fac2c4b0a2e3f91f8f0333bf78c0eb130bdd3a
saiful <saiful0687@gmail.com>
2023-11-19 16:04:04 -0500

    widget value

--- bin/dart_map.dart
+++ bin/dart_map.dart
@@ -1,6 +1,13 @@
 import 'package:dart_map/dart_map.dart' as dart_map;
-
 void main() {
+//create widget value
+  var number =Map<int, String >();
+  number[1]="One";
+  number[2]="Two";
+  number[3]="Three";
+  print(number);
+
+
   var capitals={
     "Bangladesh": "Dhaka",
     "India": "delhi",

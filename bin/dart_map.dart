1cbfbf2cf8f122389c52c11b36de8a28a8be9eba
saiful <saiful0687@gmail.com>
2023-11-19 15:53:19 -0500

    dart map

---
+++ bin/dart_map.dart
@@ -0,0 +1,12 @@
+import 'package:dart_map/dart_map.dart' as dart_map;
+
+void main() {
+  var capitals={
+    "Bangladesh": "Dhaka",
+    "India": "delhi",
+    "United States": "new york",
+    "Canada": "Toronto",
+    "Sri lanka ": "Nepal"
+  };
+  print(capitals);
+}

c45b77450ebb7f06bf015370cc7b854dc6773e63
saiful <saiful0687@gmail.com>
2023-11-30 22:09:00 -0500

    switch practice

---
+++ bin/basic_dart/switch_dart.dart
@@ -0,0 +1,15 @@
+void main() {
+  var day = "Saturday";
+  switch (day) {
+    case "Sunday":
+      print("Sunday");
+    case "Monday":
+      print("Monday");
+    case "Saturday":
+      print("Saturday");
+    case "Wednesday":
+      print("Wednesday");
+    default:
+      print("No match");
+  }
+}

ac8c17f4387b60f0e5e8a5debe37a5e44e46c605
saiful <saiful0687@gmail.com>
2023-12-02 11:26:42 -0500

    if else

---
+++ bin/basic_dart/if_else_demo.dart
@@ -0,0 +1,20 @@
+import 'package:test/scaffolding.dart';
+
+void main(){
+  int x=20;
+
+  if(x==10){
+    print("vlaue of X is good");}
+    else if(x==50){
+      print("x value is 50");
+
+  }
+    else if (x==33){
+      print("x value is 33");
+  }
+    else {
+      print("Nothing is true");
+  }
+  }
+
+

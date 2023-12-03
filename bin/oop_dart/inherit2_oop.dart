fe9bf5f0a686dab7d68171a9da38f6c7ee2c97c4
saiful <saiful0687@gmail.com>
2023-12-03 16:35:11 -0500

    inherit

---
+++ bin/oop_dart/inherit2_oop.dart
@@ -0,0 +1,29 @@
+void main(){
+Employee emp=Employee(25, "Wasim akram", "Pakistan");
+emp.Display();
+
+
+}
+class Person{
+  int age;
+  String name;
+  String college;
+  Person(this.age,this.name,this.college){
+
+
+   
+
+  }
+}
+class Employee extends Person{
+  Employee(super.age, super.name, super.college);
+  Display(){
+    print("His age $age");
+    print("His name $name");
+    print("College  was $college");
+
+    
+  }
+
+
+}

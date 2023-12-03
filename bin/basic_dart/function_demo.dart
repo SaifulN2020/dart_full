aff94db69417ac1fe840a76c1b9e2b8b53d999b4
saiful <saiful0687@gmail.com>
2023-12-02 20:13:22 -0500

    function

---
+++ bin/basic_dart/function_demo.dart
@@ -0,0 +1,18 @@
+void main(){
+  sum();
+  salay();
+}
+sum(){
+  int x=10;
+  int y=20;
+  int res=x+y;
+  print(res);
+
+}
+int salay(){
+  int man1=25;
+  int man2=35;
+  int result=man1+man2;
+  print(result);
+  return result;
+}
